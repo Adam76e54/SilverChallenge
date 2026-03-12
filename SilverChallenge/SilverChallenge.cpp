4bd57d948e5976071d00336fd200e1e94b1e3737
Adam <gillesad@gmail.com>
2026-03-12 22:08:38 +0000

    Fixed up the telemetry task using the newer silver challenge library

--- SilverChallenge/SilverChallenge.cpp
+++ SilverChallenge/SilverChallenge.cpp
@@ -11,15 +11,8 @@
 #include "Commands.h"
 #include "CD4021.h"
 
-// Declaration of Interrupt Service Routines
+// Declaration of Interrupt Service Routines and the encoder
 void leftISR(); 
-ROB12629 leftEncoder(2);
-
-// Driver is global
-L293D driver(6,7,11,12,9,10);
-
-// This needs to be shared too
-Buffer<200> outBuffer;
 
 // Tasks (for scheduler to handle)
 void telemetry(void *);
@@ -29,7 +22,7 @@ void sense(void *);
 TaskHandle_t telemetryHandle, senseHandle;
 
 // Set up semaphores (I think we just need a mutex?)
-SemaphoreHandle_t stateSemaphore, bufferSemaphore;
+SemaphoreHandle_t stateSemaphore;
 
 void setup() {
   // Set up serial for debugging
@@ -39,19 +32,14 @@ void setup() {
   wifi::initialiseAccessPoint();
 
   // Set up semaphore
-
+  stateSemaphore = xSemaphoreCreateBinary();
+  configASSERT(stateSemaphore != nullptr);
+  xSemaphoreGive(stateSemaphore); 
 
   // Set up tasks
   xTaskCreate(telemetry, "Read from GUI", 2048, nullptr, 1, &telemetryHandle);
   xTaskCreate(sense, "Sense and drive", 2048, nullptr, 2, &senseHandle);
 
-  stateSemaphore = xSemaphoreCreateBinary();
-  configASSERT(stateSemaphore != nullptr);
-  xSemaphoreGive(stateSemaphore);  // initialise to 'unlocked'
-
-  bufferSemaphore = xSemaphoreCreateBinary();
-  configASSERT(bufferSemaphore != nullptr);
-  xSemaphoreGive(bufferSemaphore);  // initialise to 'unlocked'
   // Start scheduler (does not return)
   vTaskStartScheduler();
 }
@@ -68,7 +56,7 @@ void leftISR(){
 // - TASK 1 -
 void telemetry(void *parameters){
   // Networking objects
-  Buffer<200> inBuffer;
+  Buffer<200> buffer;
   WiFiServer server(wifi::PORT);
   server.begin();
 
@@ -78,14 +66,16 @@ void telemetry(void *parameters){
     // Reconnect if disconnected
     keep(GUI, server);
     // Read a command if there is one
-    read(GUI, inBuffer);
+    read(GUI, buffer);
     // Handle a command if there is one
-    handle(inBuffer, driver);
 
-    // We've only got one thing to send right now
-    sendDistance(GUI, state::currentDistance);
+    if(xSemaphoreTake(stateSemaphore, pdMS_TO_TICKS(5)) == pdTRUE){
+      handle(buffer);
+      sendDistance(GUI, state::currentDistance);
+      xSemaphoreGive(stateSemaphore);
+    }
 
-    // Don't repeat this task again for at least 300 ms
+    // Block this task for however long
     vTaskDelay(pdMS_TO_TICKS(200));
   }
 }
@@ -97,24 +87,23 @@ void sense(void *parameters){
 
   // Hardware objects
   HCSR04 ears(8, 3); // PINS NEED TO BE CHANGED
+  ROB12629 leftEncoder(2);
   CD4021 shifter(4, 5, 13);
+  L293D driver(6,7,11,12,9,10);
 
   // Set up hardware
   driver.begin();
   ears.begin();
   shifter.begin();
-
   // We pass ISRs to the encoders
   leftEncoder.begin(leftISR);
 
   constexpr TickType_t period= pdMS_TO_TICKS(15);
   TickType_t lastWakeTime = xTaskGetTickCount();
   while(true){
-    xSemaphoreTake(stateSemaphore, portMAX_DELAY);//need to change that timeout
 
-    driver.forward(0.5, 0.5);
+    
 
-    xSemaphoreGive(stateSemaphore);
     xTaskDelayUntil(&lastWakeTime, period); 
   }
 }
