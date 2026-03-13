820c4d06e6d03397f5109a39bcc3cb6dfa0f8849
Adam <gillesad@gmail.com>
2026-03-13 16:30:51 +0000

    Added some target speed and offset

--- State/State.h
+++ State/State.h
@@ -1,10 +1,9 @@
-
 #pragma once
 
 #include <Arduino.h> 
 
 enum MODE : uint8_t {MANUAL, MAPPING};
-enum ACTIVITY : uint8_T {IDLE, FORWARD, LEFT, RIGHT};
+enum ACTIVITY : uint8_t {IDLE, FORWARD, LEFT, RIGHT};
 
 struct State{
   MODE mode = MANUAL;
@@ -16,6 +15,9 @@ struct State{
   // should be [-1.0, 1.0]
   float leftSpeedPercentage = 0;
   float rightSpeedPercentage = 0;
+
+  float targetRPM = 20;
+  float offset = 0.18;
 
   float maxDistance = 15.0;
   float currentDistance = 999.0;
