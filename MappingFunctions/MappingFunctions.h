c76857f180afe5b67ed4ca23cb451ff21453cc34
Adam <gillesad@gmail.com>
2026-03-13 16:29:17 +0000

    Added forward() with a note that it may be changed later

--- MappingFunctions/MappingFunctions.h
+++ MappingFunctions/MappingFunctions.h
@@ -1,3 +1,44 @@
 #pragma once
 
-#include <Ardu
+#include <Arduino.h>
+#include "L293D.h"
+#include "CD4021.h"
+#include "ROB12629.h"
+#include "State.h"
+
+constexpr float CIRCUMFERENCE = 20.4;
+constexpr uint8_t COUNTS_PER_REVOLUTION = 4;
+
+void forward(L293D driver, CD4021 shifter, ROB12629 encoder){
+
+  // NOTE: it actually might be better to have faith in the speed rather than the count. 
+  // If we know that she's going 20 cm/s then we can use timers which are much more accurate than encoders
+  // So I might change this later. 
+
+  // Get number of counts required
+  uint8_t targetCounts = (state.targetDistance / CIRCUMFERENCE) * COUNTS_PER_REVOLUTION;
+
+  encoder.reset();
+  auto initialShifterCount = shifter.count();
+
+  bool encoderSideDone = false, shifterSideDone = false;
+  while (!encoderSideDone || !shifterSideDone){
+    if(encoder.count() < targetCounts){
+      driver.leftForward(state.leftSpeedPercentage);
+    } else {
+      driver.leftCoast();
+      encoderSideDone = true;
+    }
+
+    if(shifter.count() - initialShifterCount < targetCounts){
+      driver.rightForward(state.rightSpeedPercentage);
+    } else {
+      driver.rightCoast();
+      shifterSideDone = true;
+    }
+  }
+}
+
+void turnLeft(L293D driver, CD4021 shifter, ROB12629 encoder){
+
+}
