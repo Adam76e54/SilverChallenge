3b4ff567f7215886ff1968c783b2ab81384ee4f6
Adam <gillesad@gmail.com>
2026-03-14 13:28:55 +0000

    Changed forward() to be based on the class internal distance tracking

--- MappingFunctions/MappingFunctions.h
+++ MappingFunctions/MappingFunctions.h
@@ -9,36 +9,39 @@
 constexpr float CIRCUMFERENCE = 20.4;
 constexpr uint8_t COUNTS_PER_REVOLUTION = 4;
 
-void forward(L293D driver, CD4021 shifter, ROB12629 encoder){
+namespace map{
+  void forward(L293D driver, CD4021 shifter, ROB12629 encoder, void (*resetShifter)()){
 
-  // NOTE: it actually might be better to have faith in the speed rather than the count. 
-  // If we know that she's going 20 cm/s then we can use timers which are much more accurate than encoders
-  // So I might change this later. 
+    // NOTE: it actually might be better to have faith in the speed rather than the count. 
+    // If we know that she's going 20 cm/s then we can use timers which are much more accurate than encoders
+    // So I might change this later. 
 
-  // Get number of counts required
-  uint8_t targetCounts = (state.targetDistance / CIRCUMFERENCE) * COUNTS_PER_REVOLUTION;
-
-  encoder.reset();
-  auto initialShifterCount = shifter.count();
-
-  bool encoderSideDone = false, shifterSideDone = false;
-  while (!encoderSideDone || !shifterSideDone){
-    if(encoder.count() < targetCounts){
-      driver.leftForward(state.leftSpeedPercentage);
-    } else {
-      driver.leftCoast();
-      encoderSideDone = true;
+    // Get number of counts required
+    encoder.reset();
+    if(resetShifter){
+      resetShifter();
     }
 
-    if(shifter.count() - initialShifterCount < targetCounts){
-      driver.rightForward(state.rightSpeedPercentage);
-    } else {
-      driver.rightCoast();
-      shifterSideDone = true;
+    bool encoderSideDone = false, shifterSideDone = false;
+    while (!encoderSideDone || !shifterSideDone){
+      shifter.update(5000); encoder.update(5000);
+      if(encoder.distance() < state.targetDistance){
+        driver.leftForward(state.leftSpeedPercentage);
+      } else {
+        driver.leftBrake(50);
+        encoderSideDone = true;
+      } 
+
+      if(shifter.distance() < state.targetDistance){
+        driver.rightForward(state.rightSpeedPercentage);
+      } else {
+        driver.rightBrake(50);
+        shifterSideDone = true;
+      }
     }
   }
-}
 
-void turnLeft(L293D driver, CD4021 shifter, ROB12629 encoder){
+  void turnLeft(L293D driver, CD4021 shifter, ROB12629 encoder){
 
+  }
 }
