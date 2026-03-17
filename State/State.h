c6a478b8add476c3f5234806d789d3c3a41b5d17
Adam <gillesad@gmail.com>
2026-03-17 16:45:52 +0000

    Added more EEPROM, removed coeffs struct, add CALIBRATING activity

--- State/State.h
+++ State/State.h
@@ -15,18 +15,30 @@ struct State{
   float totalDistance = 0;
 
   // should be [-1.0, 1.0]
-  float leftSpeedPercentage = 0;
-  float rightSpeedPercentage = 0;
+  float leftForwardPercentage = 0;
+  float rightForwardPercentage = 0;
 
-  float leftCmPerSecond = 0; 
-  float rightCmPerSecond = 0;
+  float leftBackwardPercentage = 0;
+  float rightBackwardPercentage = 0;
 
-  const uint8_t EEPROM_SIZE = 16;
-  const uint8_t LEFT_EEPROM_ADDRESS = 0;
-  const uint8_t RIGHT_EEPROM_ADDRESS = LEFT_EEPROM_ADDRESS + sizeof(float);
-  const uint8_t LEFT_CM_PER_SECOND_EPROM_ADDRESS = RIGHT_EEPROM_ADDRESS + sizeof(float);
-  const uint8_t RIGHT_CM_PER_SECOND_EPROM_ADDRESS = LEFT_CM_PER_SECOND_EPROM_ADDRESS + sizeof(float);
+  float leftForwardCmPerSecond = 0; 
+  float rightForwardCmPerSecond = 0;
+
+  float leftBackwardCmPerSecond = 0;
+  float rightBackwardCmPerSecond = 0;
+
+  const uint8_t LEFT_FORWARD_PERCENTAGE_ADDRESS = 0;
+  const uint8_t RIGHT_FORWARD_PERCENTAGE_ADDRESS = LEFT_FORWARD_PERCENTAGE_ADDRESS + sizeof(float);
+
+  const uint8_t LEFT_FORWARD_CM_PER_SECOND_ADDRESS = RIGHT_FORWARD_PERCENTAGE_ADDRESS + sizeof(float);
+  const uint8_t RIGHT_FORWARD_CM_PER_SECOND_ADDRESS = LEFT_FORWARD_CM_PER_SECOND_ADDRESS + sizeof(float);
   
+  const uint8_t LEFT_BACKWARD_PERCENTAGE_ADDRESS = RIGHT_FORWARD_CM_PER_SECOND_ADDRESS + sizeof(float);
+  const uint8_t RIGHT_BACKWARD_PERCENTAGE_ADDRESS = LEFT_BACKWARD_PERCENTAGE_ADDRESS + sizeof(float);
+
+  const uint8_t LEFT_BACKWARD_CM_PER_SECOND_ADDRESS = RIGHT_BACKWARD_PERCENTAGE_ADDRESS + sizeof(float);
+  const uint8_t RIGHT_BACKWARD_CM_PER_SECOND_ADDRESS = LEFT_BACKWARD_CM_PER_SECOND_ADDRESS + sizeof(float);
+
   float targetCmPerSecond = 20;
   float offset = 0.18;
 
